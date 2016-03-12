639b9ab80ed638171161197f4e6eebed59d52c2d
Edward Viaene <ward.viaene@bigdatapartnership.com>
2016-03-12 16:59:33 +0100

    Update install_ambari_agent.sh

--- scripts/install_ambari_agent.sh
+++ scripts/install_ambari_agent.sh
@@ -6,7 +6,7 @@ setenforce 0
 wget -nv http://public-repo-1.hortonworks.com/ambari/centos7/2.x/updates/2.2.0.0/ambari.repo -O /etc/yum.repos.d/ambari.repo
 
 # Install java-1.8
-yum -y install java-1.8.0-openjdk
+yum -y install java-1.8.0-openjdk java-1.8.0-openjdk-devel
 
 # Install ambari-server
 yum -y install ambari-agent
