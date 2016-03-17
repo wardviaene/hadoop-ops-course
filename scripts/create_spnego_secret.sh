ee94d53b1d910251d9c9a689fb333939406ee9ab
Edward Viaene <ward.viaene@gmail.com>
2016-03-17 18:48:34 +0100

    removed spnego scripts

--- scripts/create_spnego_secret.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-dd if=/dev/urandom of=/vagrant/http_secret bs=1024 count=1
--- scripts/import_spnego_secret.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp /vagrant/http_secret /etc/security/http_secret
-chown hdfs:hadoop /etc/security/http_secret
-chmod 440 /etc/security/http_secret
