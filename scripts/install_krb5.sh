0007306983169082f3c098e989f3149f670b7805
Edward Viaene <ward.viaene@gmail.com>
2016-03-17 15:42:54 +0100

    krb5 install script

---
+++ scripts/install_krb5.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+yum install krb5-server krb5-devel krb5-workstation krb5-libs rng-tools -y
+
+service rngd start
+
+echo '[logging]
+ default = FILE:/var/log/krb5libs.log
+ kdc = FILE:/var/log/krb5kdc.log
+ admin_server = FILE:/var/log/kadmind.log
+
+[libdefaults]
+ dns_lookup_realm = false
+ ticket_lifetime = 24h
+ renew_lifetime = 7d
+ forwardable = true
+ rdns = false
+ default_realm = EXAMPLE.COM
+ default_ccache_name = KEYRING:persistent:%{uid}
+
+[realms]
+ EXAMPLE.COM = {
+  kdc = node2.example.com
+  admin_server = node2.example.com
+ }
+
+[domain_realm]
+# .example.com = EXAMPLE.COM
+# example.com = EXAMPLE.COM
+'> /etc/krb5.conf
+
+kdb5_util create -P admin -s
+
+echo '*/admin@EXAMPLE.COM  *' > /var/kerberos/krb5kdc/kadm5.acl
+kadmin.local -q "addprinc -pw admin admin/admin"
+
+service krb5kdc start
+service kadmin start
