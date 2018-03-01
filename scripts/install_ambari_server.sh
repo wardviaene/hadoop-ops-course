a91fde4ea786ce51579b374160b840f2b0e76598
Edward Viaene <ward.viaene@gmail.com>
2018-03-01 16:03:43 +0100

    disable ssl verification

--- scripts/install_ambari_server.sh
+++ scripts/install_ambari_server.sh
@@ -14,6 +14,9 @@ yum -y install ambari-server
 # bootstrap ambari-server
 ambari-server setup -s --java-home=/usr/lib/jvm/jre/
 
+# disable ssl checks
+sed -i 's/verify=platform_default/verify=disable/' /etc/python/cert-verification.cfg || echo "could not disable ssl checks"
+
 # start ambari-server
 ambari-server start
 
